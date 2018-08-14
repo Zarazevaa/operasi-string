e03bc5124064f360f14873f8a9285e913d947001
ZEVA <ZEVA@LAPTOP-HTFI5LEL>
2018-08-14 13:02:24 +0700

    Coba method length & is empty

--- src/j11/praktikum1.java
+++ src/j11/praktikum1.java
@@ -13,5 +13,15 @@ public class praktikum1 {
      public static void main(String[] args){
         String identitas = "Zara Zeva Az zurra / XRPL3 / 40 ";
         System.out.println("identitas" + identitas);
+        
+         String x = "Operasi";
+        System.out.println("isi variabel x : " + x);
+        System.out.println("\"" + x + "\" panjang karakter = " + x.length());
+        
+        System.out.println("x adalah kosong : " + x.isEmpty());
+        
+        String y = "";
+        System.out.println("isi variabel y : " + y);
+        System.out.println("y adalah kosong : " + y.isEmpty());
 }
 }
