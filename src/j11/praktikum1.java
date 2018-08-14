77ce6c764249644952cb51a9922144f99fad36c7
ZEVA <ZEVA@LAPTOP-HTFI5LEL>
2018-08-14 13:00:46 +0700

    Initial commit

---
+++ src/j11/praktikum1.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package j11;
+import java.util.Scanner;
+/**
+ *
+ * @author ZEVA
+ */
+public class praktikum1 {
+     public static void main(String[] args){
+        String identitas = "Zara Zeva Az zurra / XRPL3 / 40 ";
+        System.out.println("identitas" + identitas);
+}
+}
